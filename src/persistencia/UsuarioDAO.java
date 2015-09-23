4cf0c77401e254b82663b3f7017c41b404e0f03b
Ana Flávia <flavia.chamoraes@hotmail.com>
2015-09-22 23:58:05 -0300

    Update UsuarioDAO.java

--- src/persistencia/UsuarioDAO.java
+++ src/persistencia/UsuarioDAO.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 import modelo.Usuario;
 
 public class UsuarioDAO implements DAO<Usuario> {
-	ArrayList<Usuario> filme = new ArrayList();
 	
 	public void save(Usuario object) {
 		try {
@@ -22,38 +21,4 @@ public class UsuarioDAO implements DAO<Usuario> {
 			e.printStackTrace();
 		}
 	}
-		
-		public void delete(Usuario object) {
-			try{
-				File arq = new File ("usuarios/"+object.getNumero()+".csv");
-				//se o arquivo não existe não continua.
-				if (! arq.exists()) return;
-				//exclui o arquivo.
-				arq.delete();
-			}catch (Exception e){
-				e.printStackTrace();
-			}
-		}
-		
-		public ArrayList<Usuario> findAll() {
-			ArrayList<Usuario> lista= new ArrayList();
-			try{
-				File dir = new File("Usuarios");
-				File[] arqs = dir.listFiles();
-				for (File arq: arqs){
-					Scanner scan = new Scanner(arq);
-					String linha = scan.nextLine();
-					String [] colunas = linha.split(",");
-					Usuario f = new Usuario();
-					f.setNumero(Integer.parseInt(colunas[0]));
-					f.setLogin(colunas[1]);
-					f.setEmail(colunas[2]);
-					f.setSenha(colunas[3]);
-				}
-			} catch (Exception e){
-				e.printStackTrace();
-			}
-			return lista;
-		}
-
 }
